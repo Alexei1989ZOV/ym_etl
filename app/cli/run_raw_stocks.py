250ed078dbe6f4c618219590eca02478bc10a4ab
Alexey Smotrov <avsmotrov@ya.ru>
2026-05-27 00:23:35 +0300

    добавил парсинг аргументов в run.raw_stocks

--- app/cli/run_raw_stocks.py
+++ app/cli/run_raw_stocks.py
@@ -1,5 +1,5 @@
-from datetime import date
-
+from datetime import date, datetime
+import argparse
 import logging
 from app.configs.settings import settings
 from app.storage.database import SessionLocal
@@ -10,43 +10,74 @@ from app.processing.file_manager import FileManager
 from app.core.pipeline import ReportPipeline
 from app.api.report_client import ReportAPIClient
 from app.storage.repositories.raw_stocks_repository import RawStocksRepository
-
-logging.basicConfig(
-        level=logging.INFO,
-        format='%(asctime)s - %(name)s - %(levelname)s - %(message)s'
-    )
-logger = logging.getLogger(__name__)
+from app.configs.logger_settings import setup_logging, get_logger
 
 def main():
+    setup_logging()
+    logger = get_logger("raw_stocks")
+    parser = argparse.ArgumentParser(description="Запуск отчета по остаткам.")
+    
+    # Парсим дату запуска
+    parser.add_argument(
+        "run_date", 
+        type=str, 
+        help="""Дата запуска отчета в формате ГГГГ-ММ-ДД. 
+        Вернутся данные за день, предшествующий указанной дате"""
+        )
+    
+    # Парсим опцию уровня логгирования
+    parser.add_argument(
+        "--log-level",
+        type=str,
+        default="INFO",
+        choices=["DEBUG", "INFO", "WARNING", "ERROR"],
+        help="Выбор уровня логгирования"
+    )
+    
+    # Парсим опцую пропуска загрузки если данные за дату есть в БД
+    parser.add_argument(
+        "--skip_if_exists",
+        action="store_true",
+        help="Пропустить загрузку, если данные за эту дату уже есть"
+    )
+    args = parser.parse_args()
+    run_date = datetime.strptime(args.run_date, "%Y-%m-%d").date()
+    
+    # Устанавливаем уровень логирования
+    log_level = getattr(logging, args.log_level.upper(), logging.INFO)
 
-    # 1️⃣ Подключение к БД
+    # Меняем уровень у корневого логгера и всех обработчиков
+    root_logger = logging.getLogger()
+    root_logger.setLevel(log_level)
+    for handler in root_logger.handlers:
+        handler.setLevel(log_level)
+
+    logger = get_logger("raw_stocks")
+    logger.debug(f"Установлен уровень логирования: {args.log_level}")
+    
+    # Подключение к БД
     session = SessionLocal()
 
-    # 2️⃣ Менеджер дат (начало с конкретной даты, по умолчанию до вчера)
-    date_manager = DateManager(
-        start_date=date(2026, 5, 25),
-        end_date=date(2026, 5, 25),
-    )
-
-    # 3️⃣ Файловый менеджер
+    
+    # Файловый менеджер
     file_manager = FileManager(
         raw_dir=settings.temp_dir,
         processed_dir=settings.reports_dir,
     )
 
-    # 4️⃣ API клиент
+    # API клиент
     api_client = ReportAPIClient(
         api_key=settings.api_key,
         business_id=settings.business_id,   # обязательно
         campaign_id=settings.campaign_id,   # можно None
     )
 
-    # 5️⃣ Pipeline для отчёта
+    # Pipeline для отчёта
     report_pipeline = ReportPipeline(
         api_client=api_client,
     )
 
-    # 6️⃣ ETL пайплайн
+    # ETL пайплайн
     etl_pipeline = RawStocksETLPipeline(
         session=session,
         file_manager=file_manager,
@@ -55,23 +86,25 @@ def main():
 
     repository = RawStocksRepository(session)
 
-    # 7️⃣ Оркестратор
+    # Оркестратор
     orchestrator = RawStocksOrchestrator(
         etl_pipeline=etl_pipeline,
         repository=repository,
-        start_date=date_manager.start_date,
-        end_date=date_manager.end_date,
-        skip_if_exists=False
+        start_date=run_date,
+        end_date=run_date,
+        skip_if_exists=args.skip_if_exists
     )
 
-    # 8️⃣ Пробегаем по всем датам и запускаем
-    for run_date in date_manager.get_dates():
-        try:
-            orchestrator.run_for_date(run_date)
-            logger.info(f"Отчет за {run_date} загружен")
-        except Exception as e:
-            logger.error(f"Ошибка при загрузке отчета за {run_date} : {e}")
-            continue
+    # Запуск
+    
+    try:
+        orchestrator.run_for_date(run_date)
+        logger.info(f"[RAW STOCKS] Отчет за {run_date} загружен")
+    except Exception as e:
+        logger.error(f"[RAW STOCKS] Ошибка при загрузке отчета за {run_date} : {e}")
+    finally:
+        session.close()
+    
 
 if __name__ == "__main__":
     main()
